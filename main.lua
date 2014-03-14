8f3422298333065cece3b380e91263b66388aff3
julleannem <julleannem@hotmail.com>
2014-03-14 06:48:18 -0300

    Alterações de imagens, score e na chave
    E também deletei algumas imagens.

--- main.lua
+++ main.lua
@@ -7,13 +7,15 @@ physics.start()
 display.setStatusBar( display.HiddenStatusBar )
 
 --criação dos objetos
-local background = display.newImageRect("background.jpg", 990, 580)
+local background = display.newImageRect("Muro.png", 990, 580)
 background:setFillColor(10, 10, 10)
 background:translate(display.contentCenterX, display.contentCenterY)
 background:setMask(mask)
 
-local floor = display.newImage( "floor.png", 100, 800 )
+local floor = display.newImageRect( "Chao.png", 987, 290 )
 physics.addBody( floor, "static", { friction=0.6 } )
+floor.x = 500
+floor.y = 542
 
 local porta = display.newImageRect("porta_abrindo.gif", 80, 150)
 porta.x = 900
@@ -25,39 +27,24 @@ Escada.y = 295
 Escada.rotation= -20
 
 local chave = display.newImageRect("chave002.png", 50, 70)
-chave.x = 440
-chave.y = 25
-physics.addBody( chave, { density = 1.0, bounce = 0.2 } )
-chave.isPlatform = true
-chave.isFixedRotation = true 
+chave.x = 520
+chave.y = 196
+--physics.addBody( chave, { density = 1.0, bounce = 0.2 } )
+--chave.isPlatform = true
+--chave.isFixedRotation = true 
 function chave:tap( event ) 
-chave.isVisible = true
 if(chave.tap) then
-   chave.isVisible = false
+   physics.addBody( chave, { density = 1.0, bounce = 0.2 } )
 end
 end
 
 chave:addEventListener( "tap", chave ) 
 
 
-local player = display.newImage( "2100.png", 100, 150 )
+local player = display.newImageRect( "2100.png", 100, 150 )
 player.x = 100
 player.y = 500
-
-
---center_x = display.contentCenterX
---center_y = display.contentCenterY
-
---transition.to( player, {time=1000, x = center_x, y = center_y} )
---function player:tap (event)
---	if (event.x < player.contentWidth/2) then
---		transition.to( player, {time=1000, x = center_x, y = center_y} )
-	--else 
-	--	transition.to( Escada, {time=1000})
---	end
---end
-
---player:addEventListener("tap", player)
+--physics.addBody(player)
 
 
  local buttonUp = display.newImageRect("buttonUp.png", 50, 50)
@@ -94,236 +81,278 @@ player.y = 500
  buttonEsq:addEventListener( "tap", buttonEsq ) 
  buttonDir:addEventListener( "tap", buttonDir ) 
  
-
+--PONTUAÇÃO
+local score = 0
+local scoreDisplay = display.newText("Score: ", 0, 0, nil, 30 )
+scoreDisplay.x = display.contentWidth - 910
+scoreDisplay.y = 78
 
 --Criacao de caixas
-score = 0
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Amarela (segunda fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=310
 caixa.y=125
 caixa:setFillColor(43, 70, 0)
-physics.addBody( caixa, { density = 1.0, bounce = 0.2 } )
 caixa.isPlatform = false
 caixa.isFixedRotation = true 
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 10
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Verde (segunda fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=380
 caixa.y=125
 caixa:setFillColor(0, 10, 10)
-physics.addBody( caixa, { density = 1.0, bounce = 0.3 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true 
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 30
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70, 70)
-caixa.x=540
+--Caixa Azul (segunda fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
+caixa.x=450
 caixa.y=125
 caixa:setFillColor(0, 0, 100)
-physics.addBody( caixa, { density = 1.0, bounce = 0.4 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true 
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 50
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70,70)
+--Caixa Vermelha (segunda fila)
+local caixa = display.newImageRect("caixa1.png", 70,70)
 caixa.x=520
 caixa.y=125
 caixa:setFillColor(10, 0, 0)
-physics.addBody( caixa, { density = 1.0, bounce = 0.5 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true 
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 70
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Amarela (segunda fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=590
 caixa.y=125
 caixa:setFillColor(43, 70, 0)
-physics.addBody( caixa, { density = 1.0, bounce = 0.4 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true 
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 10
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Vermelha (segunda fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=660
 caixa.y=125
 caixa:setFillColor(10, 0, 0)
-physics.addBody( caixa, { density = 2.0, bounce = 0.3 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true 
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 70
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Verde (segunda fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=730
 caixa.y=125
 caixa:setFillColor(0, 10, 10 )
-physics.addBody( caixa, { density = 2.0, bounce = 0.2 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true 
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 30
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Azul (primeira fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=310
 caixa.y=50
 caixa:setFillColor(0, 0, 100)
-physics.addBody( caixa, { density = 2.0, bounce = 0.2 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true 
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 50
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Vermelha (primeira fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=380
 caixa.y=50
 caixa:setFillColor(100, 0, 0)
-physics.addBody( caixa, { density = 2.0, bounce = 0.2 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true 
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 70
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
-
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Amarela (primeira fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=450
 caixa.y=50
 caixa:setFillColor(43, 70, 0)
-physics.addBody( caixa, { density = 2.0, bounce = 0.2 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 10
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Verde (primeira fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=520
 caixa.y=50
 caixa:setFillColor(0, 10, 10)
-physics.addBody( caixa, { density = 2.0, bounce = 0.2 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true 
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 30
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Azul (primeira fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=590
 caixa.y=50
 caixa:setFillColor(0, 0, 100)
-physics.addBody( caixa, { density = 2.0, bounce = 0.2 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 50
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Vermelha (terceira fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=310
-caixa.y=25
+caixa.y=196
 caixa:setFillColor(100, 0, 0)
-physics.addBody( caixa, { density = 2.0, bounce = 0.2 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 70
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-local caixa = display.newImageRect("caixa.png", 70, 70)
+--Caixa Amarela (terceira fila)
+local caixa = display.newImageRect("caixa1.png", 70, 70)
 caixa.x=520
-caixa.y=25
+caixa.y=196
 caixa:setFillColor(43, 70, 0)
-physics.addBody( caixa, { density = 1.0, bounce = 0.3 } )
 caixa.isPlatform = true
 caixa.isFixedRotation = true
 function caixa:tap( event ) 
 caixa.isVisible = true
 if(caixa.tap) then
    caixa.isVisible = false
+   score = score + 10
+   scoreDisplay.text = "Score: " .. score
+   scoreDisplay.anchorX = 0.5
 end
 end
 
 caixa:addEventListener( "tap", caixa ) 
 
-
---Runtime:addEventListener("enterframe", update)
 
 ---limites de tela
 local paredeesquerda = display.newRect(0,0,1, display.contentHeight + 550)
@@ -338,39 +367,3 @@ physics.addBody(paredecima, "static", {bounce = 0.1})
 ---tamanho da tela
 print("Height: ", display.contentHeight)
 print("Width: ", display.contentWidth)
-
-
---PONTUAÇÃO
-local scoreDisplay = display.newText("Score:", 0, 0, nil, 50 )
-scoreDisplay.x = display.contentWidth - 910
-scoreDisplay.y = 78
-
---function Score:tap(caixa)
---   if(caixa.tap) then
---      score = score + 70
---   end
---end
---Score:addEventListener("tap", caixa)
-
--- function scoreCaixaVermelha:tap(event)
---    if(caixa.tap) then
---       score = score + 70
---    end
--- end
-
--- scoreCaixaVermelha:addEventListener("tap", caixa)
-
--- function scoreCaixaAmarela:tap(event)
---    score = score + 30
--- end
-
--- function scoreCaixaAzul:tap(event)
---    score = score + 30
--- end
-
--- function scoreCaixaVerde:tap(event)
---    score = score + 30
--- end
-
-
-
